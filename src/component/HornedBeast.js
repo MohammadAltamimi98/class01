baf728635259564303e63956eb51ed92f000d979
Mohammad Altamimi <m98altamimi@gmail.com>
2021-04-26 00:14:19 +0300

    firsDay

---
+++ src/component/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class HornedBeast extends React.Component{
+    render(){
+
+    return(<div>
+<h2>Title: Unicorn Head</h2>
+<img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" alt="test" title="test"></img>
+<p>Beast description: Someone wearing a creepy unicorn head mask</p>
+</div>)
+};
+}
+
+export default HornedBeast;
